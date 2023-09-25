e599f481a0b6147bda04ab6c98a60c5ba0f33d97
AlexRimC9 <alexrimC9@gmail.com>
2023-09-25 12:27:02 +0300

    Commit 13 What's a Perfect Power anyway? 25.09.2023

---
+++ src/main/java/org/example/five_kyu/WhatsAPerfectPowerAnyway/Main.java
@@ -0,0 +1,10 @@
+package org.example.five_kyu.WhatsAPerfectPowerAnyway;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(WhatsAPerfectPowerAnyway.isPerfectPower(243
+        )));
+    }
+}
---
+++ src/main/java/org/example/five_kyu/WhatsAPerfectPowerAnyway/WhatsAPerfectPowerAnyway.java
@@ -0,0 +1,15 @@
+package org.example.five_kyu.WhatsAPerfectPowerAnyway;
+
+public class WhatsAPerfectPowerAnyway {
+
+    public static int[] isPerfectPower(int n) {
+        for (int i = 2; i < Math.log(n) / Math.log(2) + 1; i++) {
+            for (int j = 2; Math.pow(j, i) <= n; j++) {
+                if (Math.pow(j, i) == n) {
+                    return new int[]{j, i};
+                }
+            }
+        }
+        return null;
+    }
+}
